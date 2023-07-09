ca47c747ba90970c1fa373e50120e33a7141f450
kyle <103938273+kylechrty@users.noreply.github.com>
2023-07-08 17:16:31 -0700

    updates to nav bar and pulled changes saturday class

--- vendor-seekr/src/components/NavBar.jsx
+++ vendor-seekr/src/components/NavBar.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import vsLogo from '../assets/2.png';
-import searchIcon from '../assets/search-icon.png';
+import vsLogo from '../assets/vendorJoeLogo.png';
 import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+// nav-bar rednders diferently depending on if logged in or not
+
 
 function NavBar(props) {
-  return (
-    <>
-      <div className='nav-bar'>
-        {/* <div className="nav-content">VendorSeekr</div> */}
-        <Link to='/'>
-          <img className='logo' id='logo' src={vsLogo} />
-        </Link>
-        <div className='right-nav'>
-          <div className='search-wrap'>
-            <input type='text' id='search' placeholder='Search...' />
-            {/* <button id='search-btn'> */}
-            <img id='searchIcon' alt='lookup glass' />
-            {/* </button> */}
-          </div>
-          <div>
-            <Link to='/signup'>
-              <button className='signup-btn'>Sign Up</button>
-            </Link>
-            <Link to='/login'>
-              <button className='login-btn'>Log In</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='nav-bar'>
+				{/* <div className="nav-content">VendorSeekr</div> */}
+				<Link to='/'>
+					<img className='logo' id='logo' src={vsLogo} />
+				</Link>
+				<div className='right-nav'>
+					<div className='search-wrap'>
+						<input type='text' id='search' placeholder='Search...' />
+						<div className='search-btn'><SearchIcon /></div>
+					</div>
+					<div>
+						<Link to='/signup'>
+							<button className='signup-btn'>Sign Up</button>
+						</Link>
+						<Link to='/login'>
+							<button className='login-btn'>Log In</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default NavBar;
--- vendor-seekr/src/components/Signup.jsx
+++ vendor-seekr/src/components/Signup.jsx
@@ -1,51 +1,53 @@
 import { useState } from 'react';
 
 const Signup = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+	const handleUsernameChange = (e) => {
+		setUsername(e.target.value);
+	};
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+	const handlePasswordChange = (e) => {
+		setPassword(e.target.value);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('/api/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		fetch('/api/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username, password }),
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
 
-  return (
-    <form method='POST' action='' onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type='text'
-          name='username'
-          placeholder='username'
-          onChange={handleUsernameChange}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type='text'
-          name='password'
-          placeholder='password'
-          onChange={handlePasswordChange}
-        />
-      </label>
-      <input type='submit' value='Create Account' />
-    </form>
-  );
+	return (
+		<>
+			<form method='POST' action='' onSubmit={handleSubmit}>
+				<label>
+					Username:
+					<input
+						type='text'
+						name='username'
+						placeholder='username'
+						onChange={handleUsernameChange}
+					/>
+				</label>
+				<label>
+					Password:
+					<input
+						type='text'
+						name='password'
+						placeholder='password'
+						onChange={handlePasswordChange}
+					/>
+				</label>
+				<input type='submit' value='Create Account' />
+			</form>
+		</>
+	);
 };
 
 export default Signup;
