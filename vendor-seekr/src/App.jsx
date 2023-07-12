92df54e71e6dec492c0e3e84a6c087f7d209e668
Chandler <lcchrty@gmail.com>
2023-07-11 17:25:36 -0700

    new vendor card info

--- vendor-seekr/src/App.jsx
+++ vendor-seekr/src/App.jsx
@@ -1,37 +1,37 @@
 // import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import './App.scss';
-import NavBar from './components/NavBar';
-import CardsDisplay from './components/CardsDisplay';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import MyVendors from './components/MyVendors'
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar";
+import CardsDisplay from "./components/CardsDisplay";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import MyVendors from "./components/MyVendors";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(true);
-	const [user, setUser] = useState("tomato bob")
-	const fetchUserVerfification = () => {
-		fetch('/api/user', { method: "GET" })
-			.then(response => response.json())
-			.then(response => {
-				setIsLoggedIn(response.loggedIn) //need res.locals objects from back end?
-				setUser(response.username)
-				console.log("verified user")
-			})
-	}
-	useEffect(() => fetchUserVerfification(), [])
-	return (
-		<>
-			<NavBar loggedIn={isLoggedIn} />
-			<Routes>
-				<Route path='/' element={<CardsDisplay user={user} />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/myvendors' element={<MyVendors user={user} />} />
-			</Routes>
-		</>
-	);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState("tomato bob");
+  const fetchUserVerfification = () => {
+    fetch("/api/user", { method: "GET" })
+      .then((response) => response.json())
+      .then((response) => {
+        setIsLoggedIn(response.loggedIn); //need res.locals objects from back end?
+        setUser(response.username);
+        console.log("verified user");
+      });
+  };
+  useEffect(() => fetchUserVerfification(), []);
+  return (
+    <>
+      <NavBar loggedIn={isLoggedIn} />
+      <Routes>
+        <Route path="/" element={<CardsDisplay user={user} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/myvendors" element={<MyVendors user={user} />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- vendor-seekr/src/components/CardsDisplay.jsx
+++ vendor-seekr/src/components/CardsDisplay.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import Card from './Card.jsx';
+import React from "react";
+import Card from "./Card.jsx";
 
 const vendors = [
-	{
-		Name: 'Food Store',
-		Description: 'We sell Food!',
-		Dates: 'July 10, 2023',
-		Markets: 'Leimert Park',
-		photo:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg',
-	},
-	{
-		Name: 'Shoe Store',
-		Description: 'We sell Shoes!',
-		Dates: 'July 10, 2023',
-		Markets: 'South Street Farmers Market',
-		photo:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg',
-	},
-	{
-		Name: 'Book Store',
-		Description: 'We sell Books!',
-		Dates: 'July 16, 2023',
-		Markets: 'Silver Lake Flea',
-		photo:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg',
-	},
+  {
+    Name: "Food Store",
+    Category: "Food",
+    Description: "We sell Food!",
+    Dates: "July 10, 2023",
+    Markets: "Leimert Park",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
+  },
+  {
+    Name: "Shoe Store",
+    Category: "Clothing & Accessories",
+    Description: "We sell Shoes!",
+    Dates: "July 10, 2023",
+    Markets: "South Street Farmers Market",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
+  },
+  {
+    Name: "Book Store",
+    Description: "We sell Books!",
+    Category: "Books",
+    Dates: "July 16, 2023",
+    Markets: "Silver Lake Flea",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
+  },
 ];
 /*
 // skeleton fetch to backend >> how do we need to receive this from back end?
@@ -41,13 +44,13 @@ const vendorFetch = () => {
 */
 
 const CardsDisplay = ({ user }) => {
-	const cards = vendors.map((vendor, i) => {
-		console.log(<Card vendorInfo={vendor} user={user} key={i} />)
-		return <Card vendorInfo={vendor} user={user} key={i} />;
-	});
-	// do logic to map details as props to cards
-	// console.log(cards);
-	return <div className='card-display'>{cards}</div>;
+  const cards = vendors.map((vendor, i) => {
+    console.log(<Card vendorInfo={vendor} user={user} key={i} />);
+    return <Card vendorInfo={vendor} user={user} key={i} />;
+  });
+  // do logic to map details as props to cards
+  // console.log(cards);
+  return <div className="card-display">{cards}</div>;
 };
 
 export default CardsDisplay;
