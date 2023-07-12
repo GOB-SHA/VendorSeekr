6e8a72d61ef67ac64090028cc243d8a9d99bbcf1
Renee Toscan <renee.toscan@outlook.com>
2023-07-11 19:55:09 -0700

    signUp, getUser set up

--- vendor-seekr/server/controller/userController.js
+++ vendor-seekr/server/controller/userController.js
@@ -1,5 +1,45 @@
-const db = require('../models/db.js');
+const db = require("../models/db.js");
+const bcrypt = require("bcryptjs");
 
 const userController = {};
+
+userController.signUp = async (req, res, next) => {
+  try {
+    let hashedPW = "undefined";
+    const { first_name, last_name, email, phone, username, password } =
+      req.body;
+    await bcrypt.hash(password, 10, (err, hash) => {
+      hashedPW = hash;
+      res.locals.encrypted = hashedPW;
+    });
+
+    const addUserQuery = `INSERT INTO "user" (first_name, last_name, email, phone, username, password) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const addUserValues = [
+      first_name,
+      last_name,
+      email,
+      phone,
+      username,
+      hashedPW,
+    ];
+    await db.query(addUserQuery, addUserValues);
+    return next();
+  } catch (error) {
+    next({ log: "error in signUp middleware", message: { err: error } });
+  }
+};
+
+userController.getUser = async (req, res, next) => {
+  try {
+    const userQuery = 'SELECT * FROM "public"."user" LIMIT 100';
+    const users = await db.query(userQuery);
+    res.locals.users = users.rows;
+    next();
+  } catch (error) {
+    next({
+      message: error,
+    });
+  }
+};
 
 module.exports = userController;
--- vendor-seekr/server/routes/user.js
+++ vendor-seekr/server/routes/user.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const userController = require('../controller/userController');
 
+router.post('/signup', userController.signUp, (req, res) => {
+  return res.status(200).send('success');
+});
+
+router.get('/getuser', userController.getUser, (req, res) => {
+  return res.status(200).send(res.locals.users);
+})
+
 module.exports = router;
