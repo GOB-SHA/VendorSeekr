4c09a8e63a342c7d0a51babf45973e4bcd59c523
StephyAllezy <lilianna.goforward@gmail.com>
2023-07-11 22:14:05 -0400

    Tuesday added vendor card modal

--- vendor-seekr/src/components/Card.jsx
+++ vendor-seekr/src/components/Card.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import likeIcon from "../assets/vendorJoe.png";
+import React from 'react';
+import likeIcon from '../assets/vendorJoe.png';
+import { useState } from 'react';
 
 // handle click funciton send post request to backend to enter information from card into database
 // Category is type in the SQL database
@@ -14,35 +15,66 @@ const Card = ({ vendorInfo, user }) => {
     // 	}
     // })
   };
-  return (
-    <>
-      <div className="vendorCard">
-        <img src={vendorInfo.photo} className="vendorPhoto" />
-        <div>
-          <button className="likeBtn" onClick={handleLike}>
-            <img className="likeIcon" src={likeIcon} />
-          </button>
-        </div>
-        <div className="vendor-details">
-          <p>
-            <strong>Name: </strong>
-            {vendorInfo.Name}
-          </p>
-          <p>
-            <strong>Category: </strong>
-            {vendorInfo.Category}
-          </p>
-          <p>
-            <strong>Description: </strong>
-            {vendorInfo.Description}
-          </p>
-          <p>
-            <strong>Next Chance to Shop: </strong>
-            {vendorInfo.Dates} @ {vendorInfo.Markets}
-          </p>
-        </div>
+  const [showModal, setShowModal] = useState(false);
+  const handleModal = () => {
+    if (showModal) setShowModal(false);
+    else setShowModal(true);
+  };
+
+  return !showModal ? (
+    <div className='vendorCard' onClick={handleModal}>
+      <img src={vendorInfo.photo} className='vendorPhoto' />
+      <div>
+        <button className='likeBtn' onClick={handleLike}>
+          <img className='likeIcon' src={likeIcon} />
+        </button>
       </div>
-    </>
+      <div className='vendor-details'>
+        <p>
+          <strong>Name: </strong>
+          {vendorInfo.Name}
+        </p>
+        <p>
+          <strong>Category: </strong>
+          {vendorInfo.Category}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {vendorInfo.Description}
+        </p>
+        <p>
+          <strong>Next Chance to Shop: </strong>
+          {vendorInfo.Dates} @ {vendorInfo.Markets}
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div className='vendorCardModal' onClick={handleModal}>
+      <img src={vendorInfo.photo} className='vendorPhotoModal' />
+      <div>
+        <button className='likeBtn' onClick={handleLike}>
+          <img className='likeIcon' src={likeIcon} />
+        </button>
+      </div>
+      <div className='vendor-details'>
+        <p>
+          <strong>Name: </strong>
+          {vendorInfo.Name}
+        </p>
+        <p>
+          <strong>Category: </strong>
+          {vendorInfo.Category}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {vendorInfo.Description}
+        </p>
+        <p>
+          <strong>Next Chance to Shop: </strong>
+          {vendorInfo.Dates} @ {vendorInfo.Markets}
+        </p>
+      </div>
+    </div>
   );
 };
 
