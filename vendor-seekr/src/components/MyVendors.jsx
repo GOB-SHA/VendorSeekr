17af2aa1ee6b82b4419b270c039bc841b5dda795
Chandler <lcchrty@gmail.com>
2023-07-11 19:21:46 -0700

    email JS set up and sending data to email template

--- vendor-seekr/src/components/MyVendors.jsx
+++ vendor-seekr/src/components/MyVendors.jsx
@@ -6,11 +6,17 @@ import vendors from "./vendors";
 const MyVendors = ({ vendorInfo, user }) => {
   // const form = useRef();
   const vendorList = vendors.map((vendor) => vendor.Name);
+  const markets = vendors.map((vendor) => vendor.Markets);
+  const dates = vendors.map((vendor) => vendor.Dates);
   const templateParams = {
     email: user.email,
-    message: vendorList,
-    user: user,
+    message: vendorList.join(", "),
+    user: user.name,
+    market: markets[0],
+    date: dates[0],
   };
+
+  console.log(templateParams);
   const sendEmail = (e) => {
     e.preventDefault();
     // console.log(form.current);
@@ -33,20 +39,15 @@ const MyVendors = ({ vendorInfo, user }) => {
 
   // send a get request to the backend/database for markets like by specific user
   return (
-    console.log(vendorInfo),
-    (
-      <>
-        <h1>my vendors</h1>
+    <>
+      <h1>my vendors</h1>
+      <CardsDisplay vendorInfo={vendorInfo} user={user} />
+      <div className="send-list">
         <form onSubmit={sendEmail}>
-          <div name="message">
-            <CardsDisplay vendorInfo={vendorInfo} user={user} />
-            <input type="text" name="email" placeholder="Email Address"></input>
-          </div>
           <button type="submit">Send Me My List</button>
         </form>
-        <div></div>
-      </>
-    )
+      </div>
+    </>
   );
 };
 
--- vendor-seekr/src/components/vendors.js
+++ vendor-seekr/src/components/vendors.js
@@ -2,29 +2,29 @@ const vendors = [
   {
     Name: "Heather's Honey",
     Category: "Food",
-    Description: "We sell Food!",
+    Description: "We're the bee's knees!",
     Dates: "July 10, 2023",
     Markets: "Leimert Park",
     photo:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
   },
   {
-    Name: "Shoe City",
+    Name: "Goody Two Shoes",
     Category: "Clothing & Accessories",
-    Description: "We sell Shoes!",
+    Description: "All of our shoes have sole-mates!",
     Dates: "July 10, 2023",
     Markets: "South Street Farmers Market",
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
+      "https://images.unsplash.com/photo-1630861460368-5620bd06adfd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2676&q=80",
   },
   {
     Name: "Read 'Em and Weep",
-    Description: "We sell Books!",
     Category: "Books",
+    Description: "We sell Books!",
     Dates: "July 16, 2023",
     Markets: "Silver Lake Flea",
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Fruit_bowl.jpg/640px-Fruit_bowl.jpg",
+      "https://images.unsplash.com/photo-1550399105-c4db5fb85c18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2671&q=80",
   },
 ];
 
