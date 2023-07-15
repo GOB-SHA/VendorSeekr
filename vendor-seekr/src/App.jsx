d711799d69d870e1383e41cc2ca01612177d6963
StephyAllezy <lilianna.goforward@gmail.com>
2023-07-15 12:29:06 -0400

    .

--- vendor-seekr/src/App.jsx
+++ vendor-seekr/src/App.jsx
@@ -1,18 +1,18 @@
 // import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-import "./App.scss";
-import NavBar from "./components/NavBar";
-import CardsDisplay from "./components/CardsDisplay";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import MyVendors from "./components/MyVendors";
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './App.scss';
+import NavBar from './components/NavBar';
+import CardsDisplay from './components/CardsDisplay';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import MyVendors from './components/MyVendors';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({
-    name: "tomato bob",
-    email: "lcchrty@gmail.com",
+    name: 'tomato bob',
+    email: 'lcchrty@gmail.com',
   });
   // const fetchUserVerfification = () => {
   //   fetch("/api/user", { method: "GET" })
@@ -28,10 +28,10 @@ function App() {
     <>
       <NavBar loggedIn={isLoggedIn} />
       <Routes>
-        <Route path="/" element={<CardsDisplay user={user} />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/myvendors" element={<MyVendors user={user} />} />
+        <Route path='/' element={<CardsDisplay user={user} />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/myvendors' element={<MyVendors user={user} />} />
       </Routes>
     </>
   );
--- vendor-seekr/src/components/Card.jsx
+++ vendor-seekr/src/components/Card.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import likeIcon from "../assets/vendorJoe.png";
-import { useState } from "react";
+import React from 'react';
+import likeIcon from '../assets/vendorJoe.png';
+import { useState } from 'react';
 
 // handle click funciton send post request to backend to enter information from card into database
 // Category is type in the SQL database
@@ -22,14 +22,14 @@ const Card = ({ vendorInfo, user }) => {
   };
 
   return !showModal ? (
-    <div className="vendorCard" onClick={handleModal}>
-      <img src={vendorInfo.photo} className="vendorPhoto" />
+    <div className='vendorCard' onClick={handleModal}>
+      <img src={vendorInfo.photo} className='vendorPhoto' />
       <div>
-        <button className="likeBtn" onClick={handleLike}>
-          <img className="likeIcon" src={likeIcon} />
+        <button className='likeBtn' onClick={handleLike}>
+          <img className='likeIcon' src={likeIcon} />
         </button>
       </div>
-      <div className="vendor-details">
+      <div className='vendor-details'>
         <p>
           <strong>Name: </strong>
           {vendorInfo.vendor_name}
@@ -49,14 +49,14 @@ const Card = ({ vendorInfo, user }) => {
       </div>
     </div>
   ) : (
-    <div className="vendorCardModal" onClick={handleModal}>
-      <img src={vendorInfo.photo} className="vendorPhotoModal" />
+    <div className='vendorCardModal' onClick={handleModal}>
+      <img src={vendorInfo.photo} className='vendorPhotoModal' />
       <div>
-        <button className="likeBtn" onClick={handleLike}>
-          <img className="likeIcon" src={likeIcon} />
+        <button className='likeBtn' onClick={handleLike}>
+          <img className='likeIcon' src={likeIcon} />
         </button>
       </div>
-      <div className="vendor-details">
+      <div className='vendor-details'>
         <p>
           <strong>Name: </strong>
           {vendorInfo.Name}
